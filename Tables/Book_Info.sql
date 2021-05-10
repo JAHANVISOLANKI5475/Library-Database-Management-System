b4c0541f1c12de8b6f1661a4525ba0fb2921a993
JAHANVISOLANKI5475 <60638186+JAHANVISOLANKI5475@users.noreply.github.com>
2021-05-11 00:17:16 +0530

    Update Book_Info.sql

--- Tables/Book_Info.sql
+++ Tables/Book_Info.sql
@@ -1,1 +1,15 @@
+CREATE TABLE BOOK_INFO
+(BookID int PRIMARY KEY NOT NULL
+,Title varchar(50) NOT NULL
+,PublisherName varchar(50) NOT NULL)
 
+INSERT INTO BOOK_INFO(BookId, Title, PublisherName) VALUES (1,'The Cat Who Stole Christmas','Bill''s Publishing');
+INSERT INTO BOOK_INFO(BookId, Title, PublisherName) VALUES (2,'Something Else','Jim''s Publishing');
+INSERT INTO BOOK_INFO(BookId, Title, PublisherName) VALUES (3,'Something Else','Jim''s Publishing');
+INSERT INTO BOOK_INFO(BookId, Title, PublisherName) VALUES (4,'Book 4','Penguin Publishers');
+INSERT INTO BOOK_INFO(BookId, Title, PublisherName) VALUES (5,'This Book','Jim''s Publishing');
+INSERT INTO BOOK_INFO(BookId, Title, PublisherName) VALUES (6,'That Book','Jim''s Publishing');
+INSERT INTO BOOK_INFO(BookId, Title, PublisherName) VALUES (7,'Something Else II','Jim''s Publishing');
+INSERT INTO BOOK_INFO(BookId, Title, PublisherName) VALUES (8,'Time Warp','Bill''s Publishing');
+INSERT INTO BOOK_INFO(BookId, Title, PublisherName) VALUES (9,'Time Warp II','Bill''s Publishing');
+INSERT INTO BOOK_INFO(BookId, Title, PublisherName) VALUES (10,'Time Warp III','Bill''s Publishing');
