d9343e7849f2645b7a50e786d66d77805a640263
JAHANVISOLANKI5475 <60638186+JAHANVISOLANKI5475@users.noreply.github.com>
2021-05-11 14:25:26 +0530

    Create 2)	Book_Authors.sql

---
+++ Tables/2)	Book_Authors.sql
@@ -0,0 +1,15 @@
+CREATE TABLE BOOK_INFO.Book_Authors
+(BookID int PRIMARY KEY NOT NULL
+,AuthorName varchar(50) NOT NULL)
+
+INSERT INTO Book_Authors(BookId, AuthorName) VALUES (1,'Bill Smith');
+INSERT INTO Book_Authors(BookId, AuthorName) VALUES (2,'Bill Smith');
+INSERT INTO Book_Authors(BookId, AuthorName) VALUES	(3,'Jim Jackson');
+INSERT INTO Book_Authors(BookId, AuthorName) VALUES (4,'Jim Jackson');
+INSERT INTO Book_Authors(BookId, AuthorName) VALUES	(5,'Sally Carson');
+INSERT INTO Book_Authors(BookId, AuthorName) VALUES	(6,'Sally Carson');
+INSERT INTO Book_Authors(BookId, AuthorName) VALUES	(7,'Harry Smith');
+INSERT INTO Book_Authors(BookId, AuthorName) VALUES	(8,'Jackie Williams');
+INSERT INTO Book_Authors(BookId, AuthorName) VALUES	(9,'Carrie Wilson');
+INSERT INTO Book_Authors(BookId, AuthorName) VALUES	(10,'Carrie Wilson');
+
