25ea922a9488f3e77faf50e9047c7aef15af07fb
JAHANVISOLANKI5475 <60638186+JAHANVISOLANKI5475@users.noreply.github.com>
2021-05-13 00:24:10 +0530

    Create Book_Copies.sql

---
+++ Tables/Book_Copies.sql
@@ -0,0 +1,16 @@
+CREATE TABLE Book_Copies
+(CopiesID int PRIMARY KEY NOT NULL
+,BookID int NOT NULL
+,BranchID int NOT NULL
+,No_of_Copies int)
+
+INSERT INTO Book_Copies(CopiesID, BookID, BranchID, No_of_Copies) VALUES (1,1,1,2);
+INSERT INTO Book_Copies(CopiesID, BookID, BranchID, No_of_Copies) VALUES (2,2,1,3);
+INSERT INTO Book_Copies(CopiesID, BookID, BranchID, No_of_Copies) VALUES (3,3,1,3);
+INSERT INTO Book_Copies(CopiesID, BookID, BranchID, No_of_Copies) VALUES (4,5,1,2);
+INSERT INTO Book_Copies(CopiesID, BookID, BranchID, No_of_Copies) VALUES(5,6,1,4);
+INSERT INTO Book_Copies(CopiesID, BookID, BranchID, No_of_Copies) VALUES (6,8,1,2);
+INSERT INTO Book_Copies(CopiesID, BookID, BranchID, No_of_Copies) VALUES (7,9,1,3);
+INSERT INTO Book_Copies(CopiesID, BookID, BranchID, No_of_Copies) VALUES (8,13,1,2);
+INSERT INTO Book_Copies(CopiesID, BookID, BranchID, No_of_Copies) VALUES (9,15,1,5);
+INSERT INTO Book_Copies(CopiesID, BookID, BranchID, No_of_Copies) VALUES (10,18,1,3);
