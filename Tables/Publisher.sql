908d0598aa462debf2aeefb3a4683875971ebbc4
JAHANVISOLANKI5475 <60638186+JAHANVISOLANKI5475@users.noreply.github.com>
2021-05-12 14:17:11 +0530

    Update and rename 3)	Publisher.sql to Publisher.sql

---
+++ Tables/Publisher.sql
@@ -0,0 +1,11 @@
+CREATE TABLE Publisher
+(Name varchar(50) PRIMARY KEY NOT NULL
+,Address varchar(50) NULL
+,Phone varchar(25) NULL)
+
+INSERT INTO Publisher(Name, Address, Phone) VALUES ('Penguin Publishers','15 First St','781-555-9087');
+INSERT INTO Publisher(Name, Address, Phone) VALUES ('Jim''s Publishing','123 Fake St','986-555-4567');
+INSERT INTO Publisher(Name, Address, Phone) VALUES ('Bill''s Publishing','27 Something Ave','564-555-3576');
+INSERT INTO Publisher(Name, Address, Phone) VALUES ('Some Guy Publishers','11 Sparkle St','567-555-4379');
+INSERT INTO Publisher(Name, Address, Phone) VALUES ('Running Out of Publisher Names','8 Park Blvd','978-555-3679');
+
