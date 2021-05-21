13b3a499b87d1630851a1376e4e8cf4a2d5097a0
JAHANVISOLANKI5475 <60638186+JAHANVISOLANKI5475@users.noreply.github.com>
2021-05-21 22:17:33 +0530

    Create Borrower.sql

---
+++ Tables/Borrower.sql
@@ -0,0 +1,12 @@
+CREATE TABLE Borrower
+(CardNo varchar(25) PRIMARY KEY NOT NULL
+,Name varchar(50)
+,Address varchar(50)
+,Phone varchar(25) NOT NULL)
+
+INSERT INTO Borrower(CardNo, Name, Address, Phone) VALUES ('111-111-111-111','Jack','8 Dog St','456-555-3575');
+INSERT INTO Borrower(CardNo, Name, Address, Phone) VALUES  ('111-111-111-112','Sam','529 Secont Ave','287-555-2684');
+INSERT INTO Borrower(CardNo, Name, Address, Phone) VALUES  ('111-111-111-113','Tyler','2 Pine Ln','685-555-6227');
+INSERT INTO Borrower(CardNo, Name, Address, Phone) VALUES  ('111-111-111-114','Jared','5 Fifth Ave','426-555-8679');
+
+
