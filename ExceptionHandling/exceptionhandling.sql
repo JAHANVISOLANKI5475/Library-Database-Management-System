9896eff501493e80bf613f1badf027eb0ff6b199
notchadaddy <59438058+notchadaddy@users.noreply.github.com>
2021-05-23 16:19:56 +0530

    Create exceptionhandling.sql
    The code is performed on the BOOK_INFO59 table wherein it checks whether the ID provided is valid or not.

---
+++ ExceptionHandling/exceptionhandling.sql
@@ -0,0 +1,25 @@
+DECLARE 
+   b_id BOOK_INFO59.BookID%type := 1; 
+   b_name BOOK_INFO59.Title%type; 
+   b_addr BOOK_INFO59.PublisherName%type;  
+   ex_invalid_id  EXCEPTION; 
+BEGIN 
+   IF b_id <= 0 THEN 
+      RAISE ex_invalid_id; 
+   ELSE 
+      SELECT  Title, PublisherName INTO  b_name, b_addr 
+      FROM BOOK_INFO59 
+      WHERE BookID = b_id;
+      DBMS_OUTPUT.PUT_LINE ('Title: '||  b_name);  
+      DBMS_OUTPUT.PUT_LINE ('PublisherName: ' || b_addr); 
+   END IF; 
+
+EXCEPTION 
+   WHEN ex_invalid_id THEN 
+      dbms_output.put_line('ID must be greater than zero!'); 
+   WHEN no_data_found THEN 
+      dbms_output.put_line('No such book ID!'); 
+   WHEN others THEN 
+      dbms_output.put_line('Error!');  
+END; 
+/
